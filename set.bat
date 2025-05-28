a3825d311a1a8d2f05c27f3520fa603de15f078b
bhekinko <bhekinkosiwm1726mw@protonmail.com>
2025-05-28 17:38:45 +0500

    Update set.bat

--- set.bat
+++ set.bat
@@ -1,9 +1,8 @@
 @echo off
 set "target=%TEMP%\MyPenTest"
-set "pdfurl=https://github.com/bhekinko/test/dummy.pdf"
+set "pdfurl=https://raw.githubusercontent.com/bhekinko/test/main/dummy.pdf"
 set "pdflocal=%target%\dummy.pdf"
 
 mkdir "%target%" >nul 2>&1
 powershell -Command "Invoke-WebRequest -Uri '%pdfurl%' -OutFile '%pdflocal%'"
 start "" "%pdflocal%"
-
